55979804b0f56f3cb323e3854fa5ae760fd02c46
Ian P <i.priest@surveybe.com>
2023-09-29 15:14:35 +0100

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import RootLayout from '../components/layout';
 import Footer from '../components/Footer'
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
             <title>Seasonality</title>
           </Head>
         <Component {...pageProps} />
+        <Analytics />
         <Footer/>
     </RootLayout>
     )
